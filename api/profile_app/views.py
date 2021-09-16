4ef4fbcd5aa8ec1a902a0b546a38a539fe665e1b
borko81 <bstoilov81@gmail.com>
2021-09-16 15:51:36 +0300

    Add serialzier in profile_app

--- api/profile_app/views.py
+++ api/profile_app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http.response import Http404
+from rest_framework.views import APIView
+from rest_framework import views, status
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import ApiSerializer
+
+
+class HelloView(views.APIView):
+
+    serializer_class = ApiSerializer
+
+    def get(self, request, format=None):
+        l_message = "This is a message"
+        return Response({'message': l_message})
+
+    def post(self, request):
+        serializier = self.serializer_class(data=request.data)
+
+        if serializier.is_valid():
+            name = serializier.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message}, status=status.HTTP_201_CREATED)
+        return Response(serializier.errors, status=status.HTTP_400_BAD_REQUEST)
